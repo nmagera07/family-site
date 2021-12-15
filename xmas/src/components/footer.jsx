aef79d679e927138dcee8a5495a9bf309d79bc39
Nate <nmagera07@gmail.com>
2021-12-15 09:29:17 -0500

    added mobile pictures

--- xmas/src/components/footer.jsx
+++ xmas/src/components/footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = (props) => {
     return ( 
         <div className="footer">
-            <p>Last updated December 2021</p>
+            <p></p>
             <p>&copy; Cassie & Nate Magera 2022</p>
         </div>
      );
